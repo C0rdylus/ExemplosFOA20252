0c6753302458e9608e71f788f9ba387ec55ceefa
C0rdylus <allanrosa22@gmail.com>
2025-08-13 21:10:15 -0300

    calculo imposto

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -1,7 +1,25 @@
-﻿byte idade;
+﻿/*
+byte idade;
 string nome;
 Console.Write("digite seu nome: ");
 nome = Console.ReadLine();
 Console.Write("digite sua idade: ");
 idade = Convert.ToByte(Console.ReadLine());
 Console.WriteLine("Olá " + nome + ". a idade informada foi {0}" ,  idade);
+idade *= 2;
+Console.WriteLine($"a nova idade é {idade++}");
+Console.WriteLine($"a nova idade é {++idade}");
+*/
+//altere o programa para que solicite ao usuario informar
+//o seu salario e ao final informe o desconto de irpf de acordo
+//com o salario informado. ultilize a tabela oficial de descontos de irpf do brasil.
+
+using System.ComponentModel.Design;
+
+decimal salario;
+Console.Write("Informe Seu Salario: ");
+salario = Convert.ToDecimal(Console.ReadLine());
+if (salario < Convert.ToDecimal(2259.20)) ;
+{
+    
+}
