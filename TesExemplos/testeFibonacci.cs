e3bde6280041e94db9617572f0849d22adb62d41
C0rdylus <allanrosa22@gmail.com>
2025-10-01 20:04:38 -0300

    Adicionar classe Fibonacci e teste unitário correspondente
    Implementada a classe `Fibonacci` com o método `valorNaPosicao`, que atualmente retorna 0. Adicionado o arquivo `testeFibonacci.cs` com um teste unitário para validar o retorno do método para a posição 6 da sequência de Fibonacci, que deve ser 8. O teste utiliza o framework de testes com a anotação `[TestMethod]` e verifica a igualdade com `Assert.AreEqual`.

---
+++ TesExemplos/testeFibonacci.cs
@@ -0,0 +1,25 @@
+﻿using ConceitosBasicos.uteis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesExemplos
+{
+    public class testeFibonacci
+    {
+        [TestMethod]
+        public void testFibonacci()
+        {
+            int posicao = 6;
+            int valorEsperado = 8;
+            int valorObtido;
+            Fibonacci fibonacci = new Fibonacci();
+
+            valorObtido = fibonacci.valorNaPosicao(posicao);
+
+            Assert.AreEqual(valorEsperado, valorObtido);
+        }
+    }
+}
